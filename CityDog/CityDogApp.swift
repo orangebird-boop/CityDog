469cc3cd75c4b14cadf6aeedf000872f6eabbe20
Nora Lilla Matyassi <orangebirdboop@gmail.com>
2023-02-23 23:28:55 +0100

    Initial Commit

---
+++ CityDog/CityDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityDogApp.swift
+//  CityDog
+//
+//  Created by Nora Lilla Matyassi on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CityDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
