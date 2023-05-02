abf59270a631380ac0b9e3d41da671eb799773e2
Nora Lilla Matyassi <orangebirdboop@gmail.com>
2023-05-02 23:00:39 +0200

    Creating missing VMs

---
+++ CityDog/CityDog/ViewModels/ProfileViewModels/ModifyNameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ModifyNameViewModel.swift
+//  CityDog
+//
+//  Created by Nora Lilla Matyassi on 02/05/2023.
+//
+
+import Foundation
---
+++ CityDog/CityDog/ViewModels/ProfileViewModels/ModifyPasswordViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ModifyPasswordViewModel.swift
+//  CityDog
+//
+//  Created by Nora Lilla Matyassi on 02/05/2023.
+//
+
+import Foundation
